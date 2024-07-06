9c300e73c7fa24618a7fedafa1774183584b6f5e
taoufik273 <forever273@gmail.com>
2024-07-06 21:51:38 +0100

    fin

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ if getattr(sys, 'frozen', False):
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
-DATABASE = os.path.join(os.path.dirname(__file__), 'data/saisie.db')
+# Utiliser une variable d'environnement pour définir le chemin de la base de données
+DATABASE = os.getenv('DATABASE_PATH', os.path.join(application_path, 'data', 'saisie.db'))
 
 def get_db_connection():
     try:
